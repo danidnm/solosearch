8bbce0cd5334000d05b1f50740e021b2bfdc0032
Daniel Navarro <danidnm@gmail.com>
2026-03-08 16:48:09 +0100

    Setup of user module

---
+++ server-opensearch/api/app/User/Setup/Install.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace SoloSearch\User\Setup;
+
+use SoloSearch\Core\Model\Db;
+
+class Install
+{
+    /**
+     * @var Db
+     */
+    private Db $db;
+
+    /**
+     * @param Db $db
+     */
+    public function __construct(Db $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Install User module
+     * 
+     * @param string $version
+     */
+    public function install($version = '0.0.0')
+    {
+        $this->createUserTable();
+        $this->createTokenTable();
+    }
+
+    /**
+     * Create user table
+     */
+    private function createUserTable()
+    {
+        if ($this->db->getSchema()->hasTable('user')) {
+            return;
+        }
+
+        $this->db->createTable('user', [
+            'id' => [
+                'type' => 'autoincrement'
+            ],
+            'email' => [
+                'type' => 'string',
+                'length' => 255,
+                'unique' => true
+            ],
+            'password' => [
+                'type' => 'string'
+            ],
+            'first_name' => [
+                'type' => 'string',
+                'nullable' => true
+            ],
+            'last_name' => [
+                'type' => 'string',
+                'nullable' => true
+            ],
+            'created_at' => [
+                'type' => 'timestamp'
+            ],
+            'updated_at' => [
+                'type' => 'timestamp'
+            ]
+        ]);
+    }
+
+    /**
+     * Create token table
+     */
+    private function createTokenTable()
+    {
+        if ($this->db->getSchema()->hasTable('token')) {
+            return;
+        }
+
+        $this->db->createTable('token', [
+            'id' => [
+                'type' => 'autoincrement'
+            ],
+            'user_id' => [
+                'type' => 'integer'
+            ],
+            'token' => [
+                'type' => 'string',
+                'length' => 255
+            ],
+            'created_at' => [
+                'type' => 'timestamp'
+            ],
+            'expire_at' => [
+                'type' => 'timestamp'
+            ]
+        ]);
+    }
+}
