ab1021eff295661c87e71ce746f5d74e2378f517
Daniel Navarro <danidnm@gmail.com>
2026-03-22 15:06:21 +0100

    Cache for routes

--- server-opensearch/api/app/routes.php
+++ server-opensearch/api/app/routes.php
@@ -4,19 +4,34 @@ use Slim\App;
 
 return function (App $app) {
 
-    // Discover module routes
-    $modulesDir = __DIR__;
-    if (is_dir($modulesDir)) {
-        $dirs = scandir($modulesDir);
-        foreach ($dirs as $dir) {
-            if ($dir === '.' || $dir === '..' || !is_dir($modulesDir . '/' . $dir)) {
-                continue;
-            }
-            
-            $routeFile = $modulesDir . '/' . $dir . '/etc/routes.php';
-            if (file_exists($routeFile)) {
-                (require $routeFile)($app);
-            }
-        }
-    }
-};
+     // Discover module routes
+     $container = $app->getContainer();
+     /** @var \SoloSearch\Cache\Model\CacheRepository $cache */
+     $cache = $container->get(\SoloSearch\Cache\Model\CacheRepository::class);
+     $cacheKey = 'app_module_route_files';
+     
+     $routeFiles = $cache->get($cacheKey);
+ 
+     if ($routeFiles === null) {
+         $routeFiles = [];
+         $modulesDir = __DIR__;
+         if (is_dir($modulesDir)) {
+             $dirs = scandir($modulesDir);
+             foreach ($dirs as $dir) {
+                 if ($dir === '.' || $dir === '..' || !is_dir($modulesDir . '/' . $dir)) {
+                     continue;
+                 }
+                 
+                 $routeFile = $modulesDir . '/' . $dir . '/etc/routes.php';
+                 if (file_exists($routeFile)) {
+                     $routeFiles[] = $routeFile;
+                 }
+             }
+         }
+         $cache->set($cacheKey, $routeFiles);
+     }
+ 
+     foreach ($routeFiles as $file) {
+         (require $file)($app);
+     }
+ };
