0693763e0920aa4c826181b41eab57a14da37dbc
Daniel Navarro <danidnm@gmail.com>
2026-03-22 14:55:56 +0100

    Controllers psr compliants

--- server-opensearch/api/app/Core/Controller/BaseController.php
+++ server-opensearch/api/app/Core/Controller/BaseController.php
@@ -2,19 +2,15 @@
 
 namespace SoloSearch\Core\Controller;
  
-use Psr\Container\ContainerInterface;
 use SoloSearch\Core\Http\ResponseFactory;
  
 class BaseController
 {
-    protected ContainerInterface $container;
     protected ResponseFactory $responseFactory;
  
     public function __construct(
-        ContainerInterface $container,
         ResponseFactory $responseFactory
     ) {
-        $this->container = $container;
         $this->responseFactory = $responseFactory;
     }
 }
--- server-opensearch/api/app/Feed/Controller/Api/V1/FeedController.php
+++ server-opensearch/api/app/Feed/Controller/Api/V1/FeedController.php
@@ -15,16 +15,14 @@ class FeedController extends ApiBaseController
     private \SoloSearch\Core\Model\Config $config;
 
     /**
-     * @param \Psr\Container\ContainerInterface $container
      * @param \SoloSearch\Core\Http\ResponseFactory $responseFactory
      * @param \SoloSearch\Core\Model\Config $config
      */
     public function __construct(
-        \Psr\Container\ContainerInterface $container,
         \SoloSearch\Core\Http\ResponseFactory $responseFactory,
         \SoloSearch\Core\Model\Config $config
     ) {
-        parent::__construct($container, $responseFactory);
+        parent::__construct($responseFactory);
         $this->config = $config;
     }
 
