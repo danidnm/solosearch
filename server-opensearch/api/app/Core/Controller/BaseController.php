78b1b6553b8470f49d861943506fa2ac17593372
Daniel Navarro <danidnm@gmail.com>
2026-03-25 15:34:10 +0100

    Twig only for http controllers

--- server-opensearch/api/app/Core/Controller/BaseController.php
+++ server-opensearch/api/app/Core/Controller/BaseController.php
@@ -3,32 +3,14 @@
 namespace SoloSearch\Core\Controller;
 
 use SoloSearch\Core\Http\ResponseFactory;
-use Slim\Views\Twig;
-use Psr\Http\Message\ResponseInterface;
 
 class BaseController
 {
     protected ResponseFactory $responseFactory;
-    protected Twig $view;
 
     public function __construct(
-        ResponseFactory $responseFactory,
-        Twig $view
+        ResponseFactory $responseFactory
     ) {
         $this->responseFactory = $responseFactory;
-        $this->view = $view;
-    }
-
-    /**
-     * Render Twig template
-     * 
-     * @param ResponseInterface $response
-     * @param string $template
-     * @param array $data
-     * @return ResponseInterface
-     */
-    protected function render(ResponseInterface $response, string $template = 'default.twig', array $data = []): ResponseInterface
-    {
-        return $this->view->render($response, $template, $data);
     }
 }
--- server-opensearch/api/app/Core/Controller/Web/WebBaseController.php
+++ server-opensearch/api/app/Core/Controller/Web/WebBaseController.php
@@ -4,9 +4,34 @@
  
  use Psr\Http\Message\ResponseInterface;
  use SoloSearch\Core\Controller\BaseController;
+ use SoloSearch\Core\Http\ResponseFactory;
+ use Slim\Views\Twig;
  
  class WebBaseController extends BaseController
  {
+     protected Twig $view;
+     
+     public function __construct(
+         ResponseFactory $responseFactory,
+         Twig $view
+     ) {
+         parent::__construct($responseFactory);
+         $this->view = $view;
+     }
+     
+     /**
+      * Render Twig template
+      * 
+      * @param ResponseInterface $response
+      * @param string $template
+      * @param array $data
+      * @return ResponseInterface
+      */
+     protected function render(ResponseInterface $response, string $template = 'default.twig', array $data = []): ResponseInterface
+     {
+         return $this->view->render($response, $template, $data);
+     }
+     
      protected function htmlResponse(ResponseInterface $response, string $html, int $status = 200): ResponseInterface
     {
         return $this->responseFactory->html($response, $html, $status);
